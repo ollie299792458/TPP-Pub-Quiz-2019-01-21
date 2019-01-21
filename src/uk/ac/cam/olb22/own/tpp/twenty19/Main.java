cc82cc1a2e5f89e5a3f18bd5d64e90259f51bf9a
Oliver Black <oliver.black.uk@gmail.com>
2019-01-21 17:50:07 +0000

    Add example questions

--- src/uk/ac/cam/olb22/own/tpp/twenty19/Main.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.cam.olb22.own.tpp.twenty19;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/uk/ac/cam/olb22/own/tpp/twenty19/Question1/Question1Main.java
@@ -0,0 +1,8 @@
+package uk.ac.cam.olb22.own.tpp.twenty19.Question1;
+
+public class Question1Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
